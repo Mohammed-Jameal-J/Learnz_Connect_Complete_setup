ad92212e919bfaff0aed8a12baf857d88d92572a
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-02-09 19:11:35 +0530

    Destructuring

---
+++ JavaScipt/10_destructuring/main.js
@@ -0,0 +1,11 @@
+const numbers=[1,2,3,4]
+
+const [first,second,third,four]=numbers
+console.log(third);
+
+const user ={name:"raja",age:26}
+
+const {name,age}=user
+const udated={...user,age:50}
+console.log(udated);
+
